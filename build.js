7e63a4f637bf932377b9fc959d56f4a00991875e
Zachary Yates <zachary.yates@gmail.com>
2014-05-15 16:37:43 -0700

    changed to xml for markup data

--- build.js
+++ build.js
@@ -2,39 +2,48 @@
 // clears the output folder and builds the website
 
 var path = require("path");
-
 var fs = require("fs-extra");
 var less = require("less");
 var hbs = require("hbs");
 var CleanCSS = require("clean-css");
+var parseXml = require("xml2js").parseString;
 
 var log = function () {
   console.log.apply(this, arguments);
 };
-var sourcePath = "./src";
-var objPath = "./obj";
-var binPath = "./bin";
+
+var mkdir = function (path) {
+  try {
+    fs.mkdirsSync(path);
+  } catch (ex) {
+    //console.warn(ex);
+  }
+};
+var rmdir = function (path) {
+  try {
+    fs.removeSync(path);
+  } catch (ex) {
+    //console.warn(ex);
+  }
+};
+
+var sourcePath  = __dirname + "/src";
+var objPath     = __dirname + "/obj";
+var binPath     = __dirname + "/bin";
+var dataPath    = __dirname + "/data";
 
 var cssPath = "/assets/css";
 var imgPath = "/assets/img";
 var viewsPath = "/views";
-var dataPath = "/data";
 
 // clean the build and output
-try {
-  fs.removeSync(objPath);
-} 
-finally {
-  fs.mkdirsSync(objPath);
-  fs.mkdirsSync(objPath + cssPath);
-}
+rmdir(objPath);
+mkdir(objPath);
+mkdir(objPath + cssPath);
 
-try {
-  fs.removeSync(binPath);
-} finally {
-  fs.mkdirsSync(binPath);
-  fs.mkdirsSync(binPath + cssPath);
-}
+rmdir(binPath);
+mkdir(binPath);
+mkdir(binPath + cssPath);
 
 // process styles
 var sourceStyleFiles = fs.readdirSync(sourcePath + cssPath);
@@ -70,12 +79,12 @@ var minifiedCss = new CleanCSS().minify(sourceCss);
 fs.writeFileSync(outputCssFilePath, minifiedCss);
 
 // import header and footer
-var headSource = fs.readFileSync(sourcePath + views + "/head.html", "utf8");
-var headerSource = fs.readFileSync(sourcePath + views + "/header.html", "utf8");
-var footerSource = fs.readFileSync(sourcePath + views + "/footer.html", "utf8");
+var headSource = fs.readFileSync(sourcePath + viewsPath + "/head.html", "utf8");
+var headerSource = fs.readFileSync(sourcePath + viewsPath + "/header.html", "utf8");
+var footerSource = fs.readFileSync(sourcePath + viewsPath + "/footer.html", "utf8");
 
 // compile article template
-var articleSource = fs.readFileSync(sourcePath + views + "/article.html", "utf8");
+var articleSource = fs.readFileSync(sourcePath + viewsPath + "/article.html", "utf8");
 
 // replace header/footer includes
 articleSource = articleSource.replace("{{head}}", headSource);
@@ -84,16 +93,24 @@ articleSource = articleSource.replace("{{footer}}", footerSource);
 
 var articleTemplate = hbs.compile(articleSource);
 
+var argumentPath = "/arguments";
+mkdir(binPath + argumentPath);
+
 // load article data
-var articleFiles = fs.readdirSync(dataPath + "/articles");
+var articleFiles = fs.readdirSync(dataPath + argumentPath);
 articleFiles.forEach(function (fileName) {
-  if (path.extname(fileName) === ".json") {
-    var inputPath = dataPath + "/articles/" + fileName;
-    var outputPath = binPath + "/arguments/" + fileName.replace(".json", ".html");
+  if (path.extname(fileName) === ".xml") {
+    var inputPath = dataPath + argumentPath + "/" + fileName;
+    var outputPath = binPath + argumentPath + "/" + fileName.replace(".xml", ".html");
     var input = fs.readFileSync(inputPath, "utf8");
-    var json = JSON.parse(input);
     
-    var output = articleTemplate(json);
-    fs.writeFileSync(outputPath, output);
+    parseXml(input, function (err, result) {
+      //console.dir(result.root);
+      //console.dir(result.root.references[0].ref[0].$)
+      
+      var output = articleTemplate(result.root);
+      fs.writeFileSync(outputPath, output);
+    });
   }
 });
+
